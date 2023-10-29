50e3776e4cd06cdc3f0309dbec3ff29c6647b086
danovska01 <kristina.danovska@gmail.com>
2023-10-29 17:51:33 +0200

    days of week in numbers

--- _03_Syntax, Functions and Statements/06.Sum-of-Numbers-N…M.js
+++ _03_Syntax, Functions and Statements/06.Sum-of-Numbers-N…M.js
@@ -1,7 +1,7 @@
 function solve(input1, input2){
     let num1 = Number(input1);
     let num2 = Number(input2);
-    let result= 0;
+    let result= 0; // Initialize result to 0
 
     for(let i = num1; i<=num2; i++){
     result+=i;
@@ -9,6 +9,6 @@ function solve(input1, input2){
     return result;
 }
 
-console.log(solve(3, 5));
-console.log(solve('1', '5' ));
-console.log(solve('-8', '20'));
+// console.log(solve(3, 5));
+// console.log(solve('1', '5' ));
+// console.log(solve('-8', '20'));
