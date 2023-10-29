4ac29f034a318d136788825761605692e464b632
danovska01 <kristina.danovska@gmail.com>
2023-10-29 17:26:12 +0200

    circle area

--- _03_Syntax, Functions and Statements/03-Largest-Number.js
+++ _03_Syntax, Functions and Statements/03-Largest-Number.js
@@ -11,8 +11,8 @@ function solve (num1, num2, num3){
     result=num3;
    }
 
-   console.log('The lasrgest number is ' + result +'.');
+   console.log('The largest number is ' + result +'.');
 }
 
-solve(5, -3, 16);
-solve(-3, -5, -22.5);
+// solve(5, -3, 16);
+// solve(-3, -5, -22.5);
