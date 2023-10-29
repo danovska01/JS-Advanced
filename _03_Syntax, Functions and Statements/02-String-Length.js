91f5f9ab2678f5d378cc9b6fa1193a1ebf7dfde9
danovska01 <kristina.danovska@gmail.com>
2023-10-29 17:09:02 +0200

    first commit, tasks 1 to 3, topic syntax

---
+++ _03_Syntax, Functions and Statements/02-String-Length.js
@@ -0,0 +1,10 @@
+function solve (input1, input2, input3){
+    let sum = input1.length + input2.length + input3.length;
+    let averageLength= Math.floor(sum/3);
+    console.log(sum);
+    console.log(averageLength);
+}
+
+// solve('chocolate', 'ice cream', 'cake');
+// solve('pasta', '5', '22.3');
+
