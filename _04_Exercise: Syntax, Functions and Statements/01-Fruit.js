a31a8f2705f6018d11d722ae7a6309005277c6ac
danovska01 <kristina.danovska@gmail.com>
2023-11-01 12:49:10 +0200

    rename file

--- _04_Exercise: Syntax, Functions and Statements/01-Fruit.js
+++ _04_Exercise: Syntax, Functions and Statements/01-Fruit.js
@@ -6,10 +6,7 @@ function solve(input1, input2, input3) {
   let money = (weightInGrams / 1000) * pricePerKg;
 
   console.log(
-    `I need $${money.toFixed(2)} to buy ${(weightInGrams / 1000).toFixed(
-      2
-    )} kilograms ${fruit}.`
-  );
+    `I need $${money.toFixed(2)} to buy ${(weightInGrams / 1000).toFixed(2)} kilograms ${fruit}.`);
 }
 
 solve("orange", 2500, 1.8);
