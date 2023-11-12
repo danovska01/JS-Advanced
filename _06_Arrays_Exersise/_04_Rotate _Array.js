7f1dfa62aa458c425101996da2617e13b90d394f
danovska01 <kristina.danovska@gmail.com>
2023-11-12 18:03:04 +0200

    add information note

--- _06_Arrays_Exersise/_04_Rotate _Array.js
+++ _06_Arrays_Exersise/_04_Rotate _Array.js
@@ -1,8 +1,7 @@
 function rotate(arr, rotations){
 
     for(let i = 0; i < rotations; i++){
-        // arr.unshift(arr.pop());
-        arr.push(arr.shift());
+        arr.unshift(arr.pop()); //rotates the array to the right (moves the last element to the front).
     }
     console.log(arr.join(" "));
 
@@ -11,3 +10,6 @@ function rotate(arr, rotations){
 rotate([1, 2, 3, 4, 5], 3);
 rotate(['1', '2', '3', '4'], 2);
 rotate(['Banana', 'Orange', 'Coconut', 'Apple'], 15);
+
+
+// arr.push(arr.shift()); //rotates the array to the left (moves the first element to the end).
