6241f5bc2686b028afef3d595ec2cbd906a5ebd1
danovska01 <kristina.danovska@gmail.com>
2023-11-26 17:10:02 +0200

    From JSON to HTML Table

--- _07_Objects_Lab/_06_FromJSON_to_HTML.js
+++ _07_Objects_Lab/_06_FromJSON_to_HTML.js
@@ -1,30 +1,74 @@
-function parser (json){
-    let arr = JSON.parse(json);
-    let outputArr = ["<table>"];
-    outputArr.push(makeKeyRow(arr));
-    // arr.forEach((obj) => outputArr.push(makeValueRow(obj)));
-    //outputArr.push("</table>");
-    
-    
-    function makeKeyRow(arr){
-    
-    }
-    
-    function makeValueRow(obj){
-    
-    }
-    
-    function escapeHtml(value){
-    
-    }
-    
-    console.log(outputArr.join('\n'));
-    
-    }
-    
-    
-    parser(`[{"Name":"Stamat",
-    "Score":5.5},
-    {"Name":"Rumen",
-    "Score":6}]`
-    );
+function fromJSONToHTMLTable(input) {
+  const data = JSON.parse(input);
+  const keys = Object.keys(data[0]);
+ 
+
+  let tableHTML = "<table>\n";
+  tableHTML +=
+    "   <tr>" + keys.map((key) => `<th>${key}</th>`).join("") + "</tr>\n";
+  
+
+  for (const entry of data) {
+    tableHTML +=
+      "   <tr>" +
+      keys.map((key) => `<td>${escapeHtml(entry[key])}</td>`).join("") +
+      "</tr>\n";
+      console.log(tableHTML);
+  }
+
+  tableHTML += "</table>";
+  return tableHTML;
+}
+
+function escapeHtml(unsafe) {
+  return unsafe
+    .toString()
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+// console.log(
+//   fromJSONToHTMLTable(
+//     `[
+// {"Name":"Pesho",
+// "Score":4,
+// " Grade":8},
+// {"Name":"Gosho",
+// "Score":5,
+// " Grade":8},
+// {"Name":"Angel",
+// "Score":5.50,
+// " Grade":10}
+// ]`
+//   )
+// );
+
+// console.log(
+//   fromJSONToHTMLTable(
+//     `[
+//     {"Name":"Stamat",
+//     "Score":5.5},
+//    {"Name":"Rumen",
+//     "Score":6}
+//   ]`
+//   )
+// );
+
+console.log(
+  fromJSONToHTMLTable(
+    `[
+        {"Name":"Stamat",
+        "Score":5.5,
+        "Score2":5.5},
+        {"Name":"Stamat",
+        "Score":5.5,
+        "Score2":5.5},
+       {"Name":"Rumen",
+        "Score":6,
+        "Score2":5.5}
+      ]`
+  )
+);
