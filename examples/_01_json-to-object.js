e128a3c75fb17e389a18a2afc8c713759b6687dd
danovska01 <kristina.danovska@gmail.com>
2023-12-04 19:15:25 +0200

    add examples regards json

---
+++ examples/_01_json-to-object.js
@@ -0,0 +1,12 @@
+let jsonString = '{"name":"John","age":25,"city":"New York"}';
+
+let obj = JSON.parse(jsonString);
+
+console.log(obj);
+
+
+//JSON
+//{"name":"John"}
+
+//JavaScript
+//{name:'John'}
---
+++ examples/_02_object-to-json.js
@@ -0,0 +1,9 @@
+let obj = {
+    name: 'John',
+    age: 25,
+    city: 'New York'
+};
+
+let jsonString = JSON.stringify(obj);
+
+console.log(jsonString);
