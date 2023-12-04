e2265ba1a3fbe3b947c58735792ba069e961ac27
danovska01 <kristina.danovska@gmail.com>
2023-12-04 17:26:35 +0200

    add heroic inventory 2 and json-obj/obj-json examples

---
+++ _08_Objects_Exercise/_04_Heroic_Inventory_2.js
@@ -0,0 +1,19 @@
+function heroicInventory(inputArr) {
+  const result = [];
+
+  inputArr.forEach((el) => {
+    let [name, level, items] = el.split(" / ");
+    result.push({
+      name,
+      level: Number(level),
+      items: items ? items.split(", ") : [],
+    });
+  });
+  return JSON.stringify(result);
+}
+
+console.log(heroicInventory([
+    "Isacc / 25 / Apple, GravityGun",
+    "Derek / 12 / BarrelVest, DestructionSword",
+    "Hes / 1 / Desolator, Sentinel, Antara",
+  ]));
