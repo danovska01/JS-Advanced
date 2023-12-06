52c6aa122798b4332079237880714a7839cda399
danovska01 <kristina.danovska@gmail.com>
2023-12-06 16:47:21 +0200

    sum elements

--- _09_JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ _09_JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,14 @@
 function sumTable() {
+    let costElements = document.querySelectorAll('tr td:nth-of-type(2)');
+    
+    // Use slice to exclude the last element
+    let elementsToSum = Array.from(costElements).slice(0, -1);
 
+    let sum = elementsToSum.reduce((a, x) => {
+        let currentValue = Number(x.textContent) || 0;
+        return a + currentValue;
+    }, 0);
+  
+    let resultElement = document.getElementById('sum');
+    resultElement.textContent = sum;
 }
