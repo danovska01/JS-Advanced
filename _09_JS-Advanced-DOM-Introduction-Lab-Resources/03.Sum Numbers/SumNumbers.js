dadf98a192dd56bd918d4be9a2a36abb9c71f0ea
danovska01 <kristina.danovska@gmail.com>
2023-12-06 15:25:16 +0200

    sum dom numbers

--- _09_JS-Advanced-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
+++ _09_JS-Advanced-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,12 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let firstNumberElement = document.getElementById('num1');
+    let secondNumberElement = document.getElementById('num2');
+
+    let firstNumber = Number(firstNumberElement.value);
+    let secondNumber = Number(secondNumberElement.value);
+
+    let sum = firstNumber + secondNumber;
+
+    let resultElement = document.getElementById('sum');
+    resultElement.value = sum;
 }
