f1c16d3b2de3b0a636b3325c869ec753743c2012
danovska01 <kristina.danovska@gmail.com>
2023-12-06 15:20:26 +0200

    extract dom elements

--- _09_JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ _09_JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,28 @@
 function extractText() {
-    // TODO
+   
+    const list = document.getElementById('items');
+    const textarea = document.getElementById('result');
+
+    // Check if the list and textarea exist
+    if (list && textarea) {
+        // Collect text content of each list item
+        const listItems = list.getElementsByTagName('li');
+        const collectedText = [];
+
+        for (let i = 0; i < listItems.length; i++) {
+            collectedText.push(listItems[i].textContent.trim());
+        }
+
+        // Update the textarea with the collected text
+        textarea.value = collectedText.join('\n');
+    } else {
+        console.error('List or textarea not found.');
+    }
+}
+
+function extractText2() {
+    let ulElement = document.getElementById('items');
+
+    let textareaElement = document.getElementById('result');
+    textareaElement.textContent = ulElement.textContent;
 }
