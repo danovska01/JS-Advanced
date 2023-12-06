2d0cf15424c3532841c82d897b8868db66fe3aa6
danovska01 <kristina.danovska@gmail.com>
2023-12-06 15:08:30 +0200

    edit/replace element

--- _09_JS-Advanced-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
+++ _09_JS-Advanced-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
@@ -1,3 +1,14 @@
-function editElement() {
-    // TODO
-}
+// Function to replace occurrences of match with replacer in the text content of an element
+function editElement(element, match, replacer) {
+    // Regex solution
+    let pattern = new RegExp(match, "g");
+    element.textContent = element.textContent.replace(pattern, replacer);
+  }
+  
+  // Alternative function using while loop
+  function editElement2(element, match, replacer) {
+    while (element.textContent.includes(match)) {
+      element.textContent = element.textContent.replace(match, replacer);
+    }
+  }
+  
