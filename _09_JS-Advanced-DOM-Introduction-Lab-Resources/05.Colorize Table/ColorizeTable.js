cfea76a68643f6d16abdf12a9365673bcc573e3b
danovska01 <kristina.danovska@gmail.com>
2023-12-06 15:42:51 +0200

    colorise elements

--- _09_JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
+++ _09_JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,21 @@
 function colorize() {
-    // TODO
+    let oddRows = document.querySelectorAll('tr:nth-child(odd)');
+     // let rowElements = document.querySelectorAll('tr:nth-of-type(2n) td');
+    oddRows.forEach(row => {
+        row.style.backgroundColor = 'teal';
+    });
+}
+
+
+function colorize2() {
+    let rowElements = document.getElementsByTagName('tr');
+
+    // Convert to array
+    let rows = Array.from(rowElements);
+
+    rows.forEach((x, i) => {
+        if (i % 2 != 0) {
+            x.style.backgroundColor = 'teal';
+        }
+    });
 }
