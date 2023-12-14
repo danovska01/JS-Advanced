b609a014d328ba5bf49948a3395be61780afa670
danovska01 <kristina.danovska@gmail.com>
2023-12-14 18:24:51 +0200

    subtract

--- _10_JS_DOM_Exercise/01. Subtraction/subtract.js
+++ _10_JS_DOM_Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,12 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstElement= document.getElementById('firstNumber');
+    let secondElement= document.getElementById('secondNumber');
+    let result = document.getElementById('result');
+
+    // Extracting the numeric values from the input elements
+    let num1 = Number(firstElement.value);
+    let num2 = Number(secondElement.value);
+
+    // Performing subtraction and updating the result element
+    result.textContent = (num1 - num2).toString();
 }
