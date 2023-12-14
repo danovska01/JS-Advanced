9f801b809a4c3caa0a47d87f43dcae4ce980ab21
danovska01 <kristina.danovska@gmail.com>
2023-12-14 18:53:58 +0200

    add comments on functions

--- _10_JS_DOM_Exercise/02. Pascal or Camel Case/solution.js
+++ _10_JS_DOM_Exercise/02. Pascal or Camel Case/solution.js
@@ -19,10 +19,20 @@ function solve() {
 
   resultElement.textContent = result;
 }
+//.map((word, index) => ...): 
+//This part applies a transformation to each word in the array. 
+//For the first word (index 0), it converts the entire word to lowercase using word.toLowerCase(). 
+//For subsequent words, it capitalizes the first letter using word.charAt(0).toUpperCase() 
+//and appends the rest of the word in lowercase using word.slice(1).toLowerCase().
+//.join(''): Finally, it joins the modified words back into a single string
 
 function convertToCamelCase(input) {
   return input.split(' ').map((word, index) => index === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
 }
+
+//.map(word => ...): 
+//For each word, it capitalizes the first letter using word.charAt(0).toUpperCase() 
+//and appends the rest of the word in lowercase using word.slice(1).toLowerCase().
 
 function convertToPascalCase(input) {
   return input.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
