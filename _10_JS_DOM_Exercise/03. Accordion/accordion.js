740521ce3b76b29bb3d64a8bf745a227f1ad8082
danovska01 <kristina.danovska@gmail.com>
2023-12-15 17:00:39 +0200

    showing/hiding text in an element

--- _10_JS_DOM_Exercise/03. Accordion/accordion.js
+++ _10_JS_DOM_Exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
+
 function toggle() {
-    console.log('TODO:...');
+    let buttonElement = document.getElementsByClassName('button')[0]; // Assuming there's only one element with the class 'button'
+    let textElement = document.getElementById('extra');
+
+    if (buttonElement.textContent === 'More') {
+        textElement.style.display = 'block';
+        buttonElement.textContent = 'Less';
+    } else if (buttonElement.textContent === 'Less') {
+        textElement.style.display = 'none';
+        buttonElement.textContent = 'More';
+    }
 }
