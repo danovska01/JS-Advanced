4b397d2034a3ce03b355f297161174235324f35d
danovska01 <kristina.danovska@gmail.com>
2023-12-15 20:02:45 +0200

    seach bar

--- _10_JS_DOM_Exercise/05. Table - Search Engine/solution.js
+++ _10_JS_DOM_Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,35 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    tableElements = document.querySelectorAll("tbody tr td");
+    let inputElement = document.getElementById("searchField");
 
-   }
+    let inputText = inputElement.value.trim().toLowerCase();
+
+    // Clear previous results
+    for (element of tableElements) {
+      let rowElement = element.parentElement;
+      rowElement.classList.remove("select");
+    }
+
+    // Perform the new search
+    for (element of tableElements) {
+      let elementText = element.textContent.trim().toLowerCase();
+      if (elementText.includes(inputText) && inputText !== "") {
+        let rowElement = element.parentElement;
+        rowElement.classList.add("select");
+      }
+    }
+
+    // Clear the input field after the search
+    inputElement.value = "";
+
+    // Timeset for removal of yellow collor:
+    //  setTimeout(() => {
+    //    for (element of tableElements) {
+    //      element.parentElement.classList.remove("select");
+    //    }
+    //  }, 2000);
+  }
 }
