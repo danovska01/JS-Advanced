d6bd0adbb3eef0ac0c1311b6db331fa10f332d4c
danovska01 <kristina.danovska@gmail.com>
2023-12-16 18:12:01 +0200

    formatter; generate report - partially ready

--- _10_JS_DOM_Exercise/06. Format the Text/solution.js
+++ _10_JS_DOM_Exercise/06. Format the Text/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO
+  let inputelement= document.getElementById("input");
+  let inputText = document.getElementById("input").value;
+  let sentences = inputText
+    .split(".")
+    .filter((sentence) => sentence.trim() !== "");
+
+  let outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = ""; // Clear previous content
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    let paragraphText = sentences.slice(i, i + 3).join(". ");
+
+    paragraphText += ".";
+
+    let paragraph = `<p>${paragraphText}</p>`;
+    outputDiv.innerHTML += paragraph;
+    inputelement.value= " ";
+  }
 }
--- _10_JS_DOM_Exercise/08. Generate Report/report.js
+++ _10_JS_DOM_Exercise/08. Generate Report/report.js
@@ -1,3 +1,32 @@
 function generateReport() {
-    //TODO
-}
+    let tableHeaders = document.querySelectorAll('thead th input[type="checkbox"]:checked');
+    let tableRows = document.querySelectorAll('tbody tr');
+    let reportData = [];
+  
+    tableRows.forEach(row => {
+      let rowData = {};
+  
+      tableHeaders.forEach(checkbox => {
+        let columnName = checkbox.getAttribute('name');
+        let cellValue = row.querySelector(`td:nth-child(${getIndex(checkbox)})`).textContent.trim();
+  
+        rowData[columnName] = cellValue;
+      });
+  
+      reportData.push(rowData);
+    });
+  
+    document.getElementById('output').value = JSON.stringify(reportData, null, 2);
+  }
+  
+  function getIndex(checkbox) {
+    let index = 1;
+    let currentElement = checkbox;
+  
+    while (currentElement = currentElement.previousElementSibling) {
+      index++;
+    }
+  
+    return index;
+  }
+  
