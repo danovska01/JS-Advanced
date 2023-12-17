f296db0718363705a1848e08b05041ebd9264816
danovska01 <kristina.danovska@gmail.com>
2023-12-17 12:18:32 +0200

    decimal number to binary/hexagonal convertot

--- _10_JS_DOM_Exercise/09. Number Convertor/solution.js
+++ _10_JS_DOM_Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,32 @@
-function solve() {
+// Add your JavaScript code here
+document.getElementById('convertButton').addEventListener('click', convertNumber);
 
-    //TODO...
+function convertNumber() {
+    const fromValue = document.getElementById('selectMenuFrom').value;
+    const toValue = document.getElementById('selectMenuTo').value;
+    const inputNumber = document.getElementById('input').value;
+
+    if (!inputNumber || isNaN(inputNumber)) {
+        alert('Please enter a valid number.');
+        return;
+    }
+
+    let result;
+    if (fromValue === 'decimal') {
+        if (toValue === 'binary') {
+            result = decimalToBinary(inputNumber);
+        } else if (toValue === 'hexadecimal') {
+            result = decimalToHexadecimal(inputNumber);
+        }
+    }
+
+    document.getElementById('result').value = result;
+}
+
+function decimalToBinary(decimalNumber) {
+    return (decimalNumber >>> 0).toString(2);
+}
+
+function decimalToHexadecimal(decimalNumber) {
+    return (decimalNumber >>> 0).toString(16).toUpperCase();
 }
