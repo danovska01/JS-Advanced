266b7480248b91da2df53da48edc0add19b66a93
danovska01 <kristina.danovska@gmail.com>
2023-12-17 12:05:14 +0200

    generate report

--- _10_JS_DOM_Exercise/08. Generate Report/report.js
+++ _10_JS_DOM_Exercise/08. Generate Report/report.js
@@ -1,32 +1,33 @@
 function generateReport() {
-    let tableHeaders = document.querySelectorAll('thead th input[type="checkbox"]:checked');
-    let tableRows = document.querySelectorAll('tbody tr');
-    let reportData = [];
-  
-    tableRows.forEach(row => {
-      let rowData = {};
-  
-      tableHeaders.forEach(checkbox => {
-        let columnName = checkbox.getAttribute('name');
-        let cellValue = row.querySelector(`td:nth-child(${getIndex(checkbox)})`).textContent.trim();
-  
-        rowData[columnName] = cellValue;
-      });
-  
-      reportData.push(rowData);
+  let tableHeaders = document.querySelectorAll('thead th input[type="checkbox"]:checked');
+  let tableRows = document.querySelectorAll('tbody tr');
+  let reportData = [];
+
+  tableRows.forEach(row => {
+    let rowData = {};
+
+    tableHeaders.forEach(checkbox => {
+      let columnName = checkbox.getAttribute('name');
+      let columnIndex = getIndex(checkbox);
+      let cellValue = row.querySelector(`td:nth-child(${columnIndex})`).textContent.trim();
+
+      rowData[columnName] = cellValue;
+   
     });
-  
-    document.getElementById('output').value = JSON.stringify(reportData, null, 2);
+    reportData.push(rowData);
+  });
+
+  document.getElementById('output').value = JSON.stringify(reportData, null, 2);
+}
+
+function getIndex(checkbox) {
+  let index = 1;
+  let currentElement = checkbox.parentElement;
+  console.log(currentElement)
+
+  while (currentElement = currentElement.previousElementSibling) {
+    index++;
   }
-  
-  function getIndex(checkbox) {
-    let index = 1;
-    let currentElement = checkbox;
-  
-    while (currentElement = currentElement.previousElementSibling) {
-      index++;
-    }
-  
-    return index;
-  }
-  
+
+  return index;
+}
