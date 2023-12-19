f760be51b4619e3b4e0bae50667ecf291f0c4e4a
danovska01 <kristina.danovska@gmail.com>
2023-12-19 20:54:37 +0200

    create elemet

--- _11_JS-Advanced-DOM-Events-Lab-Resources/01. List-Of-Items/app.js
+++ _11_JS-Advanced-DOM-Events-Lab-Resources/01. List-Of-Items/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    console.log('TODO:...');
-}
+    // Get the <ul> element by its ID
+    let ulElement = document.getElementById('items');
+  
+    // Get the input element by its ID
+    let inputElement = document.getElementById('newItemText');
+  
+    // Get the value of the input element
+    let inputText = inputElement.value;
+  
+    // Create a new <li> element
+    let liElement = document.createElement('li');
+  
+    // Set the text content of the <li> element
+    liElement.textContent = inputText;
+  
+    // Append the <li> element to the <ul> element
+    ulElement.appendChild(liElement);
+  
+    // Clear the input field after adding the item
+    inputElement.value = '';
+  }
+  
