1aa3bc7b1c0afbd414b06759c7aaf9f5f4f4653a
danovska01 <kristina.danovska@gmail.com>
2023-12-19 21:58:45 +0200

    add and delete elements

--- _11_JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
+++ _11_JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
-}
+    let itemsElement = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+    let inputText = inputElement.value;
+  
+    let liElement = document.createElement('li');
+    liElement.textContent = inputText;
+  
+    let delElement = document.createElement('a');
+    delElement.textContent = '[Delete]';
+    delElement.addEventListener('click', function () {
+      // Remove the parent li when the delete link is clicked
+      itemsElement.removeChild(liElement);
+    });
+  
+    liElement.appendChild(delElement);
+    itemsElement.appendChild(liElement);
+  
+    inputElement.value = '';
+  }
+  
