f11ef1d86b3ac9e8275b0942639f8b309d617156
danovska01 <kristina.danovska@gmail.com>
2023-12-19 21:40:54 +0200

    remove element

--- _11_JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
+++ _11_JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let emailInputElement = document.querySelector('input[name="email"]');
+    let emailCellElements = document.querySelectorAll('tr td:nth-of-type(2)');
+    let resultElement = document.getElementById('result');
+
+    let emailElements = Array.from(emailCellElements);
+    let targetElement = emailElements.find(x => x.textContent == emailInputElement.value)
+
+    if (targetElement) {
+        targetElement.parentElement.remove();
+
+        resultElement.textContent = 'Deleted.';
+    } else {
+        resultElement.textContent = 'Not found.';
+    }
 }
+
