45a513dbc04c284a46059a8d745b4f51dc103122
danovska01 <kristina.danovska@gmail.com>
2023-12-28 20:41:45 +0200

    gradient on mouse move in an element

--- _11_JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
+++ _11_JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  let gradientElement = document.getElementById("gradient");
+  let resultElement = document.getElementById("result");
+
+  const gradientMouseoverHandler = (e) => {
+    let percent = Math.floor((e.offsetX / e.target.offsetWidth) * 100);
+    resultElement.textContent = `${percent}%`;
+  };
+
+  gradientElement.addEventListener("mousemove", gradientMouseoverHandler);
 }
