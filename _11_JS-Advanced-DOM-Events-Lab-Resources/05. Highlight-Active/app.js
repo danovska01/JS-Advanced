a307c030b3d36e45ff8aaf5a5b2929d3baa11655
danovska01 <kristina.danovska@gmail.com>
2023-12-28 21:17:16 +0200

    focus and blur events

--- _11_JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
+++ _11_JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
-}
+    // Get all input elements within divs
+    let inputs = document.querySelectorAll('div input');
+  
+    // Attach focus and blur event listeners to each input
+    inputs.forEach(input => {
+      input.addEventListener('focus', function () {
+        // Add "focused" class to the parent div when input is focused
+        this.parentElement.classList.add('focused');
+      });
+  
+      input.addEventListener('blur', function () {
+        // Remove "focused" class from the parent div when input loses focus
+        this.parentElement.classList.remove('focused');
+      });
+    });
+  }
+  
