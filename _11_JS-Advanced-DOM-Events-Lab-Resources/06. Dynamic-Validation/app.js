4a180b32e0324aae8a355ae45ab11c4b111133b8
danovska01 <kristina.danovska@gmail.com>
2024-01-04 17:49:49 +0200

    email validate

--- _11_JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
+++ _11_JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
@@ -1,3 +1,19 @@
 function validate() {
-    console.log('TODO:...');
+  const emailInput = document.getElementById("email");
+
+  emailInput.addEventListener("change", function () {
+    const emailValue = emailInput.value;
+
+    const emailRegex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+    //emailRegex.test(emailValue)
+    //The test() method in JavaScript is a method of the regular expression object
+
+    if (!emailRegex.test(emailValue)) {
+      emailInput.classList.add("error");
+    } else {
+      emailInput.classList.remove("error");
+      emailInput.value = "";
+    }
+  });
 }
