d7e0e3548f2ac0af60df6ee3441b3e50fd575ebf
danovska01 <kristina.danovska@gmail.com>
2024-01-05 19:56:51 +0200

    dropdown menu fill

--- _12_ JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
+++ _12_ JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
+    const newItemText = document.getElementById('newItemText').value;
+    const newItemValue = document.getElementById('newItemValue').value;
+
+    // Create a new option element
+    const newOption = document.createElement('option');
+    
+    // Set textContent and value of the new option
+    //the <option> element in HTML has both textContent and value properties.
+    newOption.textContent = newItemText;
+    newOption.value = newItemValue;
+
+    // Append the new option to the select with id "menu"
+    document.getElementById('menu').appendChild(newOption);
+
+    // Clear the input fields
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
 }
