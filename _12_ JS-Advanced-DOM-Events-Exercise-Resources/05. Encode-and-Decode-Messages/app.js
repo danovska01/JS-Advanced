57e6a5de9270f8ff73274b6e12db276a83443129
danovska01 <kristina.danovska@gmail.com>
2024-01-16 17:09:45 +0200

    encode/decode message

--- _12_ JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ _12_ JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,36 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const encodeButton = document.querySelectorAll('button')[0];
+    const decodeButton = document.querySelectorAll('button')[1];
+    const senderTextarea = document.querySelectorAll('textarea')[0];
+    const receiverTextarea = document.querySelectorAll('textarea')[1];
+
+    encodeButton.addEventListener('click', encodeMessage);
+    decodeButton.addEventListener('click', decodeMessage);
+
+    function encodeMessage() {
+        let message = senderTextarea.value;
+        let encodedMessage = '';
+
+        for (let i = 0; i < message.length; i++) {
+            let currentChar = message[i];
+            let encodedChar = String.fromCharCode(currentChar.charCodeAt(0) + 1);
+            encodedMessage += encodedChar;
+        }
+
+        receiverTextarea.value = encodedMessage;
+        senderTextarea.value = '';
+    }
+
+    function decodeMessage() {
+        let encodedMessage = receiverTextarea.value;
+        let decodedMessage = '';
+
+        for (let i = 0; i < encodedMessage.length; i++) {
+            let currentChar = encodedMessage[i];
+            let decodedChar = String.fromCharCode(currentChar.charCodeAt(0) - 1);
+            decodedMessage += decodedChar;
+        }
+
+        receiverTextarea.value = decodedMessage;
+    }
 }
