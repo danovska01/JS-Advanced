31b9fddc956255a21ec6b50e7c604c73db6d5a48
danovska01 <kristina.danovska@gmail.com>
2024-01-30 18:43:53 +0200

    next articley

--- _14_ JS-Advanced-Advanced-Functions-Exercise-Resources/08. Next Article/script.js
+++ _14_ JS-Advanced-Advanced-Functions-Exercise-Resources/08. Next Article/script.js
@@ -1,3 +1,13 @@
-function getArticleGenerator(articles) {
-    // TODO
+function getArticleGenerator(input) {
+    let articles = input;
+
+    return ()=>{
+        if(articles.length>0){
+            let container = document.getElementById('content');
+            let article = document.createElement('article');
+            let currentText = articles.shift(); //The shift() method removes the first element from an array
+            article.innerText= currentText;
+            container.appendChild(article);
+        }
+    };
 }
