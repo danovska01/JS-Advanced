d0efba3e4b712a947d3a1fe87d60e387bf1e42e3
danovska01 <kristina.danovska@gmail.com>
2024-01-30 18:10:04 +0200

    Simple Calculator

--- _14_ JS-Advanced-Advanced-Functions-Exercise-Resources/07. Simple Calculator/app.js
+++ _14_ JS-Advanced-Advanced-Functions-Exercise-Resources/07. Simple Calculator/app.js
@@ -1,7 +1,31 @@
 function calculator() {
-    // TODO:
+    let input1;
+    let input2;
+    let result;
+
+    function init(selector1, selector2, resultSelector) {
+        input1 = document.querySelector(selector1);
+        input2 = document.querySelector(selector2);
+        result = document.querySelector(resultSelector);
+        console.log('init');
+    }
+
+    function add() {
+        result.value = Number(input1.value) + Number(input2.value);
+    }
+
+    function subtract() {
+        result.value = Number(input1.value) - Number(input2.value);
+    }
+
+    return {
+        init,
+        add,
+        subtract
+    };
 }
 
-
-
+// Sample execution
+const calculate = calculator();
+calculate.init("#num1", "#num2", "#result");
 
